fbbb48a3567d19939f4ac5727410d573fc290d41
tsubaric <tommysubaric@gmail.com>
2023-01-27 21:46:55 -0600

    front_end pages added

---
+++ front_end/src/pages/Event.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import "./Event.css";
+import rectangle561 from "./assets/rectangle561.svg";
+import rectangle56 from "./assets/rectangle56.svg";
+//import Dashboard from "./components/Dashboard";
+import page5 from "./components/page5";
+const Event = () => {
+  const propsData = {
+    dashboard: {
+      dashboard: {
+        myTickets: "My Tickets\n",
+        createEvent: "Create Event\n\n",
+        search: "search",
+        events: "Events\n",
+        connectWallet: "Connect Wallet",
+      },
+    },
+  };
+  return (
+    <div className="event">
+      <page5 className="dashboard-instance-1" {...propsData.dashboard} />
+      <div className="flex-container">
+        <div className="rectangle-57">
+          <span className="event-image">Event Image</span>
+        </div>
+        <span className="lolla-pallooza-2026">LollaPallooza 2026</span>
+      </div>
+      <img className="rectangle-56" src={rectangle56} />
+      <div className="flex-container-1">
+        <img className="rectangle-56-1" src={rectangle561} />
+        <img className="rectangle-62" src={rectangle561} />
+        <img className="rectangle-58" src={rectangle561} />
+        <img className="rectangle-57-1" src={rectangle561} />
+      </div>
+      <div className="flex-container-2">
+        <img className="rectangle-63" src={rectangle561} />
+        <img className="rectangle-59" src={rectangle561} />
+        <img className="rectangle-61" src={rectangle561} />
+        <img className="rectangle-60" src={rectangle561} />
+      </div>
+    </div>
+  );
+};
+export default Event;
