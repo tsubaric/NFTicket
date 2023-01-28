446206b8b36aa713b2e0281ca1313248e3d8b978
tsubaric <tommysubaric@gmail.com>
2023-01-27 20:36:09 -0600

    Create and Event Home

---
+++ front_end/src/components/page3.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./Dashboard.css";
+import logo from "../assets/logo.svg";
+import Dashboard from "./Dashboard";
+const page3 = (props) => {
+  return (
+    <div className={`dashboard ${props.className || ""}`}>
+      <img className="logo" {...props.logo} src={logo} />
+      <Dashboard className="dashboard-instance-3" {...props.dashboard} />
+    </div>
+  );
+};
+export default page3;
