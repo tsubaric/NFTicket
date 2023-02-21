c9558c9fdc63078231d3c3f129b588b870d6e4f7
mjbrom <mjbrom10@outlook.com>
2023-02-20 19:36:19 -0600

    db connection w/ no frontend

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -1,19 +1,64 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import Box from "@mui/material/Box";
+
 const Home = () => {
-  const propsData = {
-    dashboard: {
-      dashboard: {
-        search: "search",
-        events: "Events\n",
-        myTickets: "My Tickets\n",
-        connectWallet: "Connect Wallet",
-        createEvent: "Create Event\n\n",
-      },
-    },
-  };
+  const [data, setData] = useState("");
+  // const propsData = {
+  //   dashboard: {
+  //     dashboard: {
+  //       search: "search",
+  //       events: "Events\n",
+  //       myTickets: "My Tickets\n",
+  //       connectWallet: "Connect Wallet",
+  //       createEvent: "Create Event\n\n",
+  //     },
+  //   },
+  // };
+
+  useEffect(() => {
+    console.log("test");
+    async function getResponses() {
+      const items = await fetch("/users");
+      console.log(items);
+      const data = await items.json();
+      console.log("first");
+      setData(data);
+      console.log(":test");
+      console.log(data);
+    }
+    getResponses();
+  }, []);
+
   return (
     <div className="home">
+      <Box>
+        <form
+          method="POST"
+          action="/addItem"
+          // onSubmit={() => {
+          //   alert("Item Created!");
+          // }}
+        >
+          {/* add on submit*/}
+          {/* <div className="input-container">
+                <label>Item Name</label>
+                <input
+                  name="itemName"
+                  type="text"
+                  value={this.state.itemName}
+                  onChange={(e) => {
+                    this.setState({ itemName: e.target.value });
+                  }}
+                  required
+                />
+              </div> */}
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+          <br />
+        </form>
+      </Box>
       <br />
       <br />
 
