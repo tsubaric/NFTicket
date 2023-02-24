2509453ba83f25d9b9de04c9e5fed498f59c0f79
Thomas <tombutler4@hotmail.com>
2023-02-24 14:55:32 -0600

    create function working

--- back_end/controllers/event.controller.js
+++ back_end/controllers/event.controller.js
@@ -3,12 +3,12 @@
 const db = require("../models");
 const Event = db.events;
 
+const { ethers } = require("ethers");
+
 exports.create = (req, res) => {
     // TODO: upload new event metadata, return new cid to front end
     console.log("in create")
     console.log(req['body'])
-
-    // create event with contract -> return event id
 
 
     // upload eventId.json file to ipfs -> return cid
