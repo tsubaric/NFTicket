0b7773f29903341b19de6a7355b035a08b0dd0d4
Thomas <tombutler4@hotmail.com>
2023-03-22 17:49:50 -0500

    loading events and event info

--- front_end/src/interfaces/firebase_interface.js
+++ front_end/src/interfaces/firebase_interface.js
@@ -6,7 +6,7 @@ export const getEvents = async (lastEventId) => {
   console.log("last event id: " + lastEventId);
   for (let i = 0; i < lastEventId; i++) {
     const dbRef = ref(database);
-    get(child(dbRef, `events/${i}`)).then((snapshot) => {
+    return get(child(dbRef, `events/${i}`)).then((snapshot) => {
       if (snapshot.exists()) {
         cur_events.push(snapshot.val());
       } else {
@@ -19,8 +19,6 @@ export const getEvents = async (lastEventId) => {
 
 export const getEventInfo = async (eventId) => {
   const dbRef = ref(database);
-  //will need a similar fix above
-  //need to return the get otherwise the other file doesn't get the data from this function
   return get(child(dbRef, `events/${eventId}`))
     .then((snapshot) => {
       if (snapshot.exists()) {
