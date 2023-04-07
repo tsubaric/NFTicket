60a2ca1bacec0c7c9b45856e3014f791aeee59fa
Thomas <tombutler4@hotmail.com>
2023-04-07 17:11:28 -0500

    safetransferfrom function already inherited in the contract

--- contracts/NFTicket.sol
+++ contracts/NFTicket.sol
@@ -57,46 +57,5 @@ contract NFTicket is ERC1155 {
     function getGATicketsPrice (uint256 eventId) public view returns (uint256) {
         return ALL_GA_TICKETS_PRICE[eventId];
     }
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public virtual override {
-        require(
-            from == _msgSender() || isApprovedForAll(from, _msgSender()),
-            "ERC1155: caller is not token owner or approved"
-        );
-        _safeTransferFrom(from, to, id, amount, data);
-    }
-    function _safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) internal virtual {
-        require(to != address(0), "ERC1155: transfer to the zero address");
-
-        address operator = _msgSender();
-
-        _beforeTokenTransfer(operator, from, to, _asSingletonArray(id), _asSingletonArray(amount), data);
-
-        uint256 fromBalance = _balances[id][from];
-        require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
-        unchecked {
-            _balances[id][from] = fromBalance - amount;
-        }
-        _balances[id][to] += amount;
-        
-        if (!isOwner(to)) {
-            owners.push(to);
-        }
-
-        emit TransferSingle(operator, from, to, id, amount);
-
-        _doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
-    }
 
 }
