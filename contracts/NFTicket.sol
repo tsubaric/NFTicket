d44106b879bd16c19eaad2cf0494106f664a0b45
Thomas <tombutler4@hotmail.com>
2023-04-07 23:33:45 -0500

    updated contract tp give each ticket a unique id

--- contracts/NFTicket.sol
+++ contracts/NFTicket.sol
@@ -6,20 +6,38 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol"; // NFT token standar
 
 contract NFTicket is ERC1155 {
 
-    uint256 private LAST_EVENT_ID = 0;
-    uint256 constant MAX_EVENT_ID = 999;
-    uint256[] private ALL_GA_TICKETS_AVAILABLE = new uint256[](MAX_EVENT_ID);
-    uint256[] private ALL_GA_TICKETS_PRICE = new uint256[](MAX_EVENT_ID);
+    using Counters for Counters.Counter;
+    Counters.Counter private eventCounter;
+    mapping(uint256 => Counters.Counter) private ticketCounters; // counter for each event
+
+    struct Event {
+        uint256 eventId; // 1 - 999
+        uint256 ticketAmount; // 0 - 999999
+        uint256 ticketPrice;
+        uint256 ticketsAvailable;
+        address eventOwner;
+    }
+
+    struct Ticket {
+        uint256 ticketId; // ticketId is the eventId concatenated with the ticket number
+        bool redeemed;
+    }
+
+    mapping (uint256 => Event) private allEventsMap; // event id -> Event struct
+    mapping (uint256 => Ticket) private allTicketsMap;  // ticket id -> Ticket struct
+    mapping (address => uint256[]) private ownedTicketsMap;  // user -> owned ticketIds
+    mapping (address => uint256[]) private ownedEventsMap; // user -> owned eventId
+
 
     constructor() ERC1155("https://ipfs.io/ipfs/QmZHERTYLSB3EaTzmwCMXYkdSNvwfZm6ZLctoP7JrsDHma?filename={id}.json") {
-
+        eventCounter.increment(); // start at event id 1
     }
 
     // create a new event
-    function createEvent (uint256 ga_ticket_amount, uint256 ga_ticket_price) public returns (uint256) {
-        uint256 eventId = generateId();
-        ALL_GA_TICKETS_AVAILABLE[eventId] = ga_ticket_amount;
-        ALL_GA_TICKETS_PRICE[eventId] = ga_ticket_price;
+    function createEvent (uint256 ticketAmount, uint256 ticketPrice) public returns (uint256) {
+        uint256 eventId = eventCounter.current();
+        eventCounter.increment(); // start ticket ids at 1
+        allEventsMap[eventId] =  Event(eventId, ticketAmount, ticketPrice, ticketAmount, msg.sender);
         return eventId;
     }
 
@@ -27,35 +45,62 @@ contract NFTicket is ERC1155 {
         _setURI(eventURI);
     }
 
-    function mintGATickets (uint256 eventId, uint256 amount) public payable {
+    function mintTickets (uint256 eventId, uint256 amount) public payable {
         // TODO: make this actually charge for the tickets
         //require(msg.value == ALL_GA_TICKETS_PRICE[eventId] * amount, "Incorrect Amount of ETH");
-        require(ALL_GA_TICKETS_AVAILABLE[eventId] >= amount, "Insufficient tickets remaining");
+        require(allEventsMap[eventId].ticketsAvailable >= amount, "Insufficient tickets remaining");
         require(amount > 0, "Must mint at least 1 ticket");
 
-        // mint ticket and update amount available
-        // NOTE: each GA ticket is more like a fungile token than an NFT
-        _mint(msg.sender, eventId, amount, "");
-        ALL_GA_TICKETS_AVAILABLE[eventId] = ALL_GA_TICKETS_AVAILABLE[eventId] - amount;
+        // mint tickets and update amount available
+        uint256[] memory ids = new uint256[](amount);
+        uint256[] memory amounts = new uint256[](amount);
+        uint256 curTicketCount = ticketCounters[eventId].current();
+        // generate ids for minting
+        for (uint i = 0; i < amount; i++) {
+            uint256 ticketId = generateTicketId(eventId, curTicketCount);
+            ids[i] = ticketId;
+            amounts[i] = 1;
+            curTicketCount++; // incrementing temp variable because we want to increment counter after minting
+        }
+        _mintBatch(msg.sender, ids, amounts, "");
+
+        // update owned tickets
+        // doing this after minting in case mint fails
+        for (uint i = 0; i < amount; i++){
+            ticketCounters[eventId].increment(); // increment ticket counter
+            allTicketsMap[ids[i]] = Ticket(ids[i], false);  // update ticket information
+            ownedTicketsMap[msg.sender].push(ids[i]);  // add ticket to owner mapping
+        }
     }
 
     // increment the event id each time a new event is created
-    function generateId () private returns (uint256) {
-        uint256 newId = LAST_EVENT_ID + 1;
-        LAST_EVENT_ID = newId;
-        return newId;
+    function generateTicketId (uint256 eventId, uint256 ticketNumber) internal pure returns (uint256) {
+        require(eventId <= 999, "Event ID must be less than 1000");
+        require(ticketNumber <= 999999, "Ticket number must be less than 99999999");
+
+        return eventId * 1000000 + ticketNumber;
     }
 
+    // ------------------------
+    // getters defined below
     function getLastEventId () public view returns (uint256) {
-        return LAST_EVENT_ID;
+        return eventCounter.current() - 1;
     }
 
-    function getGATicketsAvailable (uint256 eventId) public view returns (uint256) {
-        return ALL_GA_TICKETS_AVAILABLE[eventId];
+    function getTicketsAvailable (uint256 eventId) public view returns (uint256) {
+        return allEventsMap[eventId].ticketsAvailable;
     }
 
-    function getGATicketsPrice (uint256 eventId) public view returns (uint256) {
-        return ALL_GA_TICKETS_PRICE[eventId];
+    function getTicketPrice (uint256 eventId) public view returns (uint256) {
+        return allEventsMap[eventId].ticketPrice;
+    }
+
+    function getEventInfo (uint256 eventId) public view returns (Event memory) {
+        return allEventsMap[eventId];
+    }
+
+    function getAllOwnedTickets () public view returns (uint256[] memory ticketIds) {
+        return ownedTicketsMap[msg.sender];
     }
 
 }
--- test/NFTicket.js
+++ test/NFTicket.js
@@ -2,19 +2,58 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 
 describe("NFTicket", async function() {
-  it("should create an event", async function() {
+    let nfticket; // reference to the contract
 
-    // deploy ticket contract
-    const NFTicket = await hre.ethers.getContractFactory("NFTicket");
-    const nfticket = await NFTicket.deploy();
+    beforeEach(async function() {
+        // deploy NFTicket contract
+        const NFTicket = await hre.ethers.getContractFactory("NFTicket");
+        nfticket = await NFTicket.deploy();
+        await nfticket.deployed();
+    })
 
-    // create events
-    await nfticket.createEvent(10, 1000) // price, amount
-    await nfticket.createEvent(10, 1000) // price, amount
+    it("should let a user create an event", async function() {
 
-    // get last event id
-    const lastEventId = await nfticket.getLastEventId();
-    expect(lastEventId.toNumber()).to.equal(2)
-  });
+        // create event
+        const tx = await nfticket.createEvent(1000, 10) // amount, price
+        await tx.wait()
+
+        // get event info from contract
+        const lastEventId = await nfticket.getLastEventId();
+        const ticketsAvailable = await nfticket.getTicketsAvailable(lastEventId);
+        const ticketPrice = await nfticket.getTicketPrice(lastEventId);
+
+        expect(lastEventId).to.equal(1);  // events start at id 1
+        expect(ticketsAvailable).to.equal(1000);
+        expect(ticketPrice).to.equal(10);
+
+    });
+
+    it("should let a user mint tickets", async function () {
+        // create event
+        await nfticket.createEvent(1000, 10);
+        await nfticket.createEvent(1000, 10); // want to event id to be nonzero
+
+        // mint a ticket
+        [user] = await ethers.getSigners();
+        let tx = await nfticket.mintTickets(1, 3) // buying 3 from first event
+        await tx.wait();
+        tx = await nfticket.mintTickets(2, 2); // buying 2 tickets from second event
+        await tx.wait();
+
+        // check user balance
+        const ownedTickets = await nfticket.getAllOwnedTickets();
+        //for (let i = 0; i < ownedTickets.length; i++){
+            //console.log(ownedTickets[i].toNumber());
+        //}
+
+        // ticket id of form    000 | 00000000
+        //                  event id    ticket id
+        expect(ownedTickets.length).to.equal(5);
+        expect(ownedTickets[0].toNumber()).to.equal(1000000);
+        expect(ownedTickets[4].toNumber()).to.equal(2000001);
+
+
+
+    })
 
 });
