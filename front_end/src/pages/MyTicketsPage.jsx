c7280a2229a974a817948e5c83b076bc2e33892b
Thomas <tombutler4@hotmail.com>
2023-05-04 17:42:55 -0500

    switch between active and redeemed on my tickets page

--- front_end/src/pages/MyTicketsPage.jsx
+++ front_end/src/pages/MyTicketsPage.jsx
@@ -8,9 +8,13 @@ import PersonIcon from "@mui/icons-material/Person";
 import Tab from "@mui/material/Tab";
 import TabList from "@mui/lab/TabList";
 import { getAllOwnedTickets, getTicketInfo, getTicketUri } from "../interfaces/NFTicket_interface";
+import TabPanel from "@mui/lab/TabPanel";
 
 export default function MyTicketsPage() {
   const [ownedTickets, setOwnedTickets] = useState([]);
+  const [value, setValue] = React.useState('1');
+  const [redeemedTickets, setRedeemedTickets] = useState([]);
+  const [activeTickets, setActiveTickets] = useState([]);
 
   async function loadTickets() {
     // get ticket ids of owned tickets
@@ -22,40 +26,78 @@ export default function MyTicketsPage() {
       }
     })
     const ownedTickets = await Promise.all(promises)
+    let activeTickets = []
+    let redeemedTickets = []
+    ownedTickets.forEach((ticket) => {
+        if (ticket.redeemed) {
+            redeemedTickets.push(ticket)
+        } else {
+            activeTickets.push(ticket)
+        }
+    })
     console.log("found owned tickets: ", ownedTickets)
-    setOwnedTickets(ownedTickets)
+    //setOwnedTickets(ownedTickets)
+    setActiveTickets(activeTickets)
+    setRedeemedTickets(redeemedTickets)
   }
 
   useEffect(() => {
     loadTickets();
   }, []);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
 
   return (
     <div className="main-container">
       <div className="ownedNFTS">
-        <TabContext value="1">
+        <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList>
-              <Tab id="tab1" icon={<PersonIcon />} label="OWNED" value="1" />
+            <TabList onChange={handleChange}>
+              <Tab id="tab1" icon={<PersonIcon />} label="ACTIVE" value="1" />
+              <Tab id="tab1" icon={<PersonIcon />} label="REDEEMED" value="2" />
             </TabList>
           </Box>
-          <Box style={{ display: "flex", justifyContent: "center", margin: "50px" }}>
-              { ownedTickets[0] === 0 ? <h1>No tickets found</h1> :
-                  ownedTickets.map((t, i) => {
-                      return (
-                        <div style={{ padding: "15px"}}>
-                        <TicketCard
-                          key={i}
-                          ticketId={t.ticketId}
-                          eventId={t.eventId}
-                          eventName={t.eventName}
-                        />
-                        </div>
-                      )
-                  })
-              }
-          </Box>
+          <TabPanel value="1">
+              <Box style={{ display: "flex", justifyContent: "center", margin: "50px" }}>
+                  { activeTickets[0] === 0 ? <h1>No tickets found</h1> :
+                      activeTickets.map((t, i) => {
+                          return (
+                            <div style={{ padding: "15px"}}>
+                            <TicketCard
+                              key={i}
+                              ticketId={t.ticketId}
+                              eventId={t.eventId}
+                              eventName={t.eventName}
+                              redeemed={t.redeemed}
+                            />
+                            </div>
+                          )
+                      })
+                  }
+              </Box>
+          </TabPanel>
+          <TabPanel value="2">
+              <Box style={{ display: "flex", justifyContent: "center", margin: "50px" }}>
+                  { redeemedTickets[0] === 0 ? <h1>No tickets found</h1> :
+                      redeemedTickets.map((t, i) => {
+                          return (
+                            <div style={{ padding: "15px"}}>
+                            <TicketCard
+                              key={i}
+                              ticketId={t.ticketId}
+                              eventId={t.eventId}
+                              eventName={t.eventName}
+                              redeemed={t.redeemed}
+                            />
+                            </div>
+                          )
+                      })
+                  }
+              </Box>
+          </TabPanel>
         </TabContext>
       </div>
     </div>
