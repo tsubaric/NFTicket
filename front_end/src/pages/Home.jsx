6f9a1060a25fba740bf5d9049fb29f7dc8505d01
Thomas <tombutler4@hotmail.com>
2023-05-04 13:21:08 -0500

    removed buttons

--- front_end/src/pages/Home.jsx
+++ front_end/src/pages/Home.jsx
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Home.css";
-import Stack from "@mui/material/Stack";
-import IconButton from "@mui/material/IconButton";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Home = () => {
   return (
@@ -32,22 +26,6 @@ const Home = () => {
             manage your upcoming events{" "}
           </p>
         </span>
-      </div>
-      <div className="mediaButtons">
-        <Stack id="mediaButtonsStack" direction="row" spacing={2}>
-          <IconButton id="twitterButton" aria-label="Twitter" size="large">
-            <TwitterIcon />
-          </IconButton>
-          <IconButton id="instaButton" aria-label="Instagram" size="large">
-            <InstagramIcon />
-          </IconButton>
-          <IconButton id="facebookButton" aria-label="Facebook" size="large">
-            <FacebookIcon />
-          </IconButton>
-          <IconButton id="youtubeButton" aria-label="YouTube" size="large">
-            <YouTubeIcon />
-          </IconButton>
-        </Stack>
       </div>
     </div>
   );
